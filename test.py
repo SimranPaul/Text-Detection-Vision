c15bdc9e76cbecc2d0e630676d7c98f321ed2d0e
Simran Paul <34294879+SimranPaul@users.noreply.github.com>
2019-07-01 14:26:44 +0530

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import json
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument('file')
-args = parser.parse_args()
-with open(args.file) as file:
-        datastore = json.load(file)
-print (datastore["office"]["parking"]["style"])
